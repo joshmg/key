1a242f7927dcd5182e14fb38ad7d36173b4cfa57
Joshua Green <joshmg@gmail.com>
2010-09-30 01:50:14 -0400

    Added pie-chart and grouping system.

--- key.cpp
+++ key.cpp
@@ -8,6 +8,8 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <process.h>
+#include <windows.h>
 using namespace std;
 
 const column TRANS_DATE_COL   = column("date", 30);
@@ -15,11 +17,21 @@ const column TRANS_TYPE_COL   = column("type", 30);
 const column TRANS_DESC_COL   = column("description", 75);
 const column TRANS_AMOUNT_COL = column("amount", 30);
 
+const column TRANS_GRP_NAME_COL = column("name", 50);
+const column TRANS_GRP_DESC_COL = column("description", 75);
+
 const string TRANSACTION_MARKER = "<!-- end isLoc or isFdr if statement -->";
 const string END_TRANSACTION_MARKER = "<!-- end for loop -->";
 
+map<float, string> pie;
+
+int _argc;
+char** _argv;
+
+void glbranch(void*);
+
 struct transaction {
-  string date, type, description;
+  string date, type, raw_description, description;
   float amount;
 
   row to_row() const {
@@ -29,15 +41,19 @@ struct transaction {
 
 transaction get_transaction(fileio&);
 string strip_whitespace(const string&);
-string to_words(const string&, int words=3);
+string format_desc(const string&, int words=3);
 
-int main() {
+int main(int argc, char** argv) {
+  _argc = argc;
+  _argv = argv;
+
   fileio data;
   database db;
 
   db.initialize("KeyBank");
 
   table* TRANSACTIONS = db.get_table("transactions");
+  table* TRANSACTION_GROUPS = db.get_table("transaction_groups");
 
   if (TRANSACTIONS == 0) {
     cout << "Creating transaction table...";
@@ -50,6 +66,15 @@ int main() {
   }
   else cout << "Transaction table loaded." << endl;
 
+  if (TRANSACTION_GROUPS == 0) {
+    cout << "Creating transaction-group table...";
+    TRANSACTION_GROUPS = db.add_table("transaction_groups");
+    TRANSACTION_GROUPS->add_column(TRANS_GRP_NAME_COL);
+    TRANSACTION_GROUPS->add_column(TRANS_GRP_DESC_COL);
+    cout << " done." << endl;
+  }
+  else cout << "Transaction-group table loaded." << endl;
+
   string input;
   cout << "Load new transactions? (y/n) ";
   getline(cin, input);
@@ -57,6 +82,12 @@ int main() {
     string filename;
     cout << "Filename: ";
     getline(cin, filename);
+
+    bool define_groups = false;
+    input.clear();
+    cout << "Define transactions groups? (y/n) ";
+    getline(cin, input);
+    define_groups = (input[0] == 'y' || input[0] == 'Y');
 
     data.open(filename, "r");
     if (!data.is_open()) {
@@ -74,6 +105,16 @@ int main() {
     while (data.pos() < stop_pos) {
       transaction t = get_transaction(data);
       TRANSACTIONS->add_row(t.to_row());
+      if (define_groups) {
+        // if a group is not already defined...
+        if (TRANSACTION_GROUPS->select(query().where(predicate().add(equalto(TRANS_GRP_DESC_COL, format_desc(t.description))))).size() == 0) {
+          input.clear();
+          cout << "\"" << t.raw_description << "\"" << endl;
+          cout << "Group: " << endl;
+          getline(cin, input);
+          TRANSACTION_GROUPS->add_row(row().add(TRANS_GRP_NAME_COL, input).add(TRANS_GRP_DESC_COL, format_desc(t.description)));
+        }
+      }
     }
     cout << " done." << endl << endl;
   }
@@ -87,8 +128,16 @@ int main() {
   float total_expenses = 0.0f, total_credits = 0.0f;
   map<string, float> totals;
   for (int i=0;i<results.size();i++) {
-    if (totals.find(to_words(results[i][TRANS_DESC_COL])) == totals.end()) totals[to_words(results[i][TRANS_DESC_COL])] = 0.0f;
-    totals[to_words(results[i][TRANS_DESC_COL])] += atof(results[i][TRANS_AMOUNT_COL].c_str());
+    // search for matching group association:
+    vector<row> associations = TRANSACTION_GROUPS->select(query().where(predicate().add(equalto(TRANS_GRP_DESC_COL, format_desc(results[i][TRANS_DESC_COL])))));
+    if (associations.size() > 0) {
+      if (totals.find(associations[0][TRANS_GRP_NAME_COL]) == totals.end()) totals[associations[0][TRANS_GRP_NAME_COL]] = 0.0f;
+      totals[associations[0][TRANS_GRP_NAME_COL]] += atof(results[i][TRANS_AMOUNT_COL].c_str());
+    }
+    else {
+      if (totals.find(format_desc(results[i][TRANS_DESC_COL])) == totals.end()) totals[format_desc(results[i][TRANS_DESC_COL])] = 0.0f;
+      totals[format_desc(results[i][TRANS_DESC_COL])] += atof(results[i][TRANS_AMOUNT_COL].c_str());
+    }
 
     if (atof(results[i][TRANS_AMOUNT_COL].c_str()) < 0) total_expenses += atof(results[i][TRANS_AMOUNT_COL].c_str());
     else total_credits += atof(results[i][TRANS_AMOUNT_COL].c_str());
@@ -105,9 +154,49 @@ int main() {
     cout << it->second << "\t(";
 
     if (it->second >= 0)  cout << floor((it->second/total_credits)*1000.0f + 0.5f)/10.0f << "%)" << endl;
-    else                  cout << floor((it->second/total_expenses)*1000.0f + 0.5f)/10.0f << "%)" << endl;
+    else {
+                          cout << floor((it->second/total_expenses)*1000.0f + 0.5f)/10.0f << "%)" << endl;
+      pie[fabs(floor((it->second/total_expenses)*1000.0f + 0.5f)/10.0f)/100.0f] = it->first;
+    }
 
     it++;
+  }
+
+  // ---------------------- OPENGL ---------------------- //
+  _beginthread(&glbranch, 0, (void*)0);
+  // -------------------- END OPENGL -------------------- //
+
+  input.clear();
+  cout << "Expand group? (y/n) ";
+  getline(cin, input);
+  if (input[0] == 'y' || input[0] == 'Y') {
+    cout << "Group: ";
+    input.clear();
+    getline(cin, input);
+    vector<row> groups = TRANSACTION_GROUPS->select(query().where(predicate().And(equalto(TRANS_GRP_NAME_COL, input))));
+    for (int groups_index=0;groups_index< groups.size();groups_index++) {
+      string group_description = groups[groups_index][TRANS_GRP_DESC_COL];
+      vector<row> transactions = TRANSACTIONS->select(query().where(predicate().And(equalto(TRANS_DESC_COL, group_description))));
+
+      for (int c=0;c<transactions.size();c++) {
+        cout << "  ";
+        string transaction_description = transactions[c][TRANS_DESC_COL];
+        float   transaction_amount = atof(transactions[c][TRANS_AMOUNT_COL].c_str());
+
+        for (int i=0;i<30;i++) {
+          if (i < transaction_description.length()) cout << transaction_description[i];
+          else cout << " ";
+        }
+
+        cout << ": ";
+        if (transaction_amount >= 0) cout << " ";
+        cout << transaction_amount << "\t(";
+
+        if (transaction_amount >= 0)  cout << floor((transaction_amount/total_credits)*1000.0f + 0.5f)/10.0f << "%)" << endl;
+        else                  cout << floor((transaction_amount/total_expenses)*1000.0f + 0.5f)/10.0f << "%)" << endl;
+
+      }
+    }
   }
 
   return 0;
@@ -122,7 +211,8 @@ transaction get_transaction(fileio& data) {
   data.read(-1, " >");
   entry.type = strip_whitespace(data.read(-1, "</td>"));
   data.read(-1, " >");
-  entry.description = strip_whitespace(data.read(-1, "</td>"));
+  entry.raw_description = data.read(-1, "</td>");
+  entry.description = format_desc(entry.raw_description);
 
   data.read(-1, "align=\"right\">");
   temp = data.read(-1, "</td>");
@@ -164,18 +254,19 @@ string strip_whitespace(const string& t) {
   return str;
 }
 
-string to_words(const string& t, int words) {
+string format_desc(const string& t, int words) {
   string str;
 
   int i=0;
   while (i < t.length()) {
     if (t[i] == ' ') words--;
     if (words > 0) {
-      str += t[i];
+      // is alpha or numeric
+      if (t[i] == ' ' || (t[i] > 64 && t[i] < 91) || (t[i] > 96 && t[i] < 123)) str += t[i];
     }
     else break;
     i++;
   }
 
-  return str;
+  return strtolower(strip_whitespace(str));
 }
---
+++ key_gui.cpp
@@ -0,0 +1,101 @@
+// File: key_gui.cpp
+// Written by Joshua Green
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <cmath>
+#include <GL/glu.h>
+#include <GL/glut.h>
+using namespace std;
+
+extern int _argc;
+extern char** _argv;
+
+float WIDTH = 600.0f;
+float HEIGHT = 300.0f;
+
+extern map<float, string> pie;
+
+struct point2d {
+  double x, y;
+
+  point2d(double _x, double _y) {x = _x; y = _y;}
+};
+struct point3d {
+  double x, y, z;
+
+  point3d(double _x, double _y, double _z) {x = _x; y = _y; z = _z;}
+};
+
+void draw_circle(float r, const point2d pos, const point3d& color, bool filled=true) {
+  float to_deg = 3.14159f/180.0f;
+  glPushMatrix();
+  glColor3f(color.x, color.y, color.z);
+  if (filled) glBegin(GL_TRIANGLE_FAN);
+  else glBegin(GL_LINE_LOOP);
+  for (int theta=0;theta<360;theta+=5) {
+    glVertex2f(pos.x + sin(theta*to_deg) * r, pos.y + cos(theta*to_deg) * r);
+  }
+  glEnd();
+  glPopMatrix();
+}
+
+void draw_pie_piece(float percent, float r, point2d pos, point3d color, float offset) {
+  float to_deg = 3.14159f/180.0f;
+
+  glPushMatrix();
+  glTranslatef(pos.x, pos.y, 0.0f);
+  glRotatef(360*offset, 0.0f, 0.0f, 1.0f);
+  glColor3f(color.x, color.y, color.z);
+  glBegin(GL_POLYGON);
+  glVertex2f(0, 0);
+  glVertex2f(r, 0); // first boundary line
+  for (int theta=0;theta<360*percent;theta+=5) {
+    glVertex2f(cos(theta*to_deg)*r, sin(theta*to_deg)*r);
+  }
+  glVertex2f(r*cos(2*3.14159*percent), r*sin(2*3.14159*percent)); // second boundary line
+
+  glEnd();
+  glPopMatrix();
+}
+
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT);         // reset background to black
+
+  //draw_circle(100.0f, point2d(WIDTH/2.0, HEIGHT/2.0), point3d(0.3, 0.3, 0.3f));
+
+  float offset = 0.0f;
+  map<float, string>::iterator pie_it = pie.begin();
+  while (pie_it != pie.end()) {
+    draw_pie_piece(pie_it->first, 100.0f, point2d(WIDTH/2.0, HEIGHT/2.0), point3d(1.0*offset, 1.0*offset, 1.0f*offset), offset);
+    offset += pie_it->first;
+    pie_it++;
+  }
+
+  glFlush(); //Send the 2D scene to the screen
+}
+
+void init_opengl() {
+  glClearColor(0.0, 0.0, 0.0, 1.0);    // black background
+
+  // Set projection matrix
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0.0, WIDTH, 0.0, HEIGHT);  // Project into 2D.
+
+  // Set modelview matrix
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+void glbranch(void*) {
+  glutInit(&_argc, _argv);               // Initialize glut from argv[]
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Single buffer, rgb color.
+  glutInitWindowSize(WIDTH, HEIGHT);         // 500 x 500 pixel window.
+  glutCreateWindow("OpenGL");          // window title
+  glutDisplayFunc(display);            // Set display callback function.
+  init_opengl();                       // Set attributes.          
+
+  glutMainLoop();                      // Enter event loop.
+}
